2c4787982a62ac4f74214cacb7e793a3834436ea
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-09 10:01:40 -0500

    pushing up app framework

---
+++ server.js
@@ -0,0 +1,28 @@
+//*** Dependencies ***//
+//====================//
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+//*** Express set up ***//
+//======================//
+var PORT = process.env.PORT || 8080;
+const app = express();
+
+//*** Middleware ***//
+//==================// 
+app.use(logger("dev"));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//*** Routers ***//
+//===============//
+// app.use(require("./routes/html-routes.js"));
+// app.use(require("./routes/api-routes.js"));
+
+//*** Listener ***//
+//================//
+app.listen(PORT, () => { console.log(`App running on port ${PORT}!`) });
